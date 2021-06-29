8d435a0ddc2468874fc4d9598900aa4ebbb16fff
Colk-tech <colk.tech@gmail.com>
2021-06-29 09:57:25 +0900

    [add] Complete "exe2-3.c"

---
+++ exe2/exe2-3.c
@@ -0,0 +1,14 @@
+//
+// Created by Manaki ITO on 2021/06/29.
+//
+#include <stdio.h>
+
+#define STRING_LENGTH 4096
+
+
+int main(void) {
+    char char_array[STRING_LENGTH];
+
+    scanf("%s", char_array);
+    printf("%s\n", char_array);
+}
