a682003b333bda9cd38d1e982d3818af3cc39003
Colk-tech <colk.tech@gmail.com>
2021-06-29 09:53:11 +0900

    [add] Complete "exe2-1.c"

---
+++ exe2/exe2-1.c
@@ -0,0 +1,13 @@
+//
+// Created by Manaki ITO on 2021/06/29.
+//
+#include <stdio.h>
+
+#define STRING_LENGTH 4
+#define STRING_CONTENT "abc"
+
+
+int main(void) {
+    char char_array[STRING_LENGTH] = STRING_CONTENT;
+    printf("%s\n", char_array);
+}
