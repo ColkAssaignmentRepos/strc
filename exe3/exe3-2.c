90344e8bd60ff82655875a1e41a43f48f322cc2a
Colk-tech <colk.tech@gmail.com>
2021-06-29 16:06:20 +0900

    [add] Complete "exe3-2.c"

---
+++ exe3/exe3-2.c
@@ -0,0 +1,30 @@
+//
+// Created by Manaki ITO on 2021/06/29.
+//
+#include <stdio.h>
+#include <string.h>
+
+#define STRING_LENGTH 4096
+
+size_t mystrlen(const char *s);
+
+
+int main(void) {
+    char char_array[STRING_LENGTH];
+
+    scanf("%s", char_array);
+    printf("LENGTH: %ld\n", mystrlen(char_array));
+
+    return 0;
+}
+
+
+size_t mystrlen(const char *s) {
+    size_t count = 0;
+
+    while (s[count] != '\0') {
+        count++;
+    }
+
+    return count;
+}
